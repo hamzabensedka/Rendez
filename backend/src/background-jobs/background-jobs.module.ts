0a3213b71bd0572cef269965f6cb80c01c0f62f3
opc <bensedkahamza@gmail.com>
2026-07-04 00:43:59 +0000

    [autocrew] backend_developer: parallel work

--- backend/src/background-jobs/background-jobs.module.ts
+++ backend/src/background-jobs/background-jobs.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { PrismaService } from '../prisma/prisma.service';
+import { NotificationsService } from '../notifications/notifications.service';
+import { FirebaseMessagingService } from '../notifications/firebase-messaging.service';
+import { EmailService } from '../notifications/email.service';
+import { BookingRemindersJob } from './jobs/booking-reminders.job';
 
-@Module({})
+@Module({
+  imports: [ScheduleModule.forRoot()],
+  providers: [
+    PrismaService,
+    NotificationsService,
+    FirebaseMessagingService,
+    EmailService,
+    BookingRemindersJob,
+  ],
+})
 export class BackgroundJobsModule {}
