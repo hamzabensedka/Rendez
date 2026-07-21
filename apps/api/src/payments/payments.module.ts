597f75985ff9b5f87179fe24752733f1ac2f2247
opc <bensedkahamza@gmail.com>
2026-07-21 01:34:48 +0000

    [autocrew] devops_engineer: parallel work

--- apps/api/src/payments/payments.module.ts
+++ apps/api/src/payments/payments.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule],
   controllers: [PaymentsController],
 })
 export class PaymentsModule {}
